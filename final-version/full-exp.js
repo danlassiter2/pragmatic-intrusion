5424358e00673d45cd9f4ee7fcd425cfc60dbbb5
Dan Lassiter <dan.lassiter@ed.ac.uk>
2025-06-30 14:09:22 -0400

    bug fixes

--- final-version/full-exp.js
+++ final-version/full-exp.js
@@ -100,22 +100,21 @@ function save_pragdep_data_line(data) {
     var data_to_save = [
         participant_id,
         data.condition,
+        data.qud,
         data.response_format, // slider or radio buttons (this will also be clear from response)
         data.block, // training or test
         data.training_trial_counter,
         data.test_trial_counter,
-        data.trial_index,
+        data.trial_index, // not in trial.data object ... 
         data.target_truth_value,
         data.all_TVs,
         data.target_content_type,
-        //data.linguistic_prompt,
+        data.linguistic_prompt,
         data.target_image,
-        data.qud,
         ...data.images_in_order, // saves all images in the presented order (0-3). The ... is called spread, is applied within another array to make them into elements in the top level array (instead of a nested array). Ex: [...[1,2],3]=[1,2,3]. Avoids issue with the quotation loop below, as would otherwise apply "" around the whole array images_in_order (and we want this array to be split for readability in the csv file later)
-        data.response,
+        data.response, 
         data.time_elapsed,
-        data.rt,
-        data.stimulus
+        data.rt
     ];
 
     // add quotation marks around each element that is saved to avoid splitting prompts that have commas
@@ -382,7 +381,6 @@ function make_training_trial(prompt, target, filler_1, filler_2, filler_3) {
                 target_image: target_filename,
                 images_in_order: images, // saves the filenames in the order they were presented in a trial, i.e. the shuffled order
                 // NOTE Not really necessary for training block, but might as well save it
-                stimulus: "NA"
             };
         },
         on_finish: function (data) {
@@ -418,8 +416,7 @@ function make_training_trial(prompt, target, filler_1, filler_2, filler_3) {
                 qud: "NA",
                 linguistic_prompt: prompt, 
                 target_image: target_filename,
-                images_in_order: images,
-                stimulus: "NA" 
+                images_in_order: images
             };
         },
         on_finish: function (data) {
@@ -622,7 +619,6 @@ function make_test_trial(target_content_type) {
         }
         all_TVs.push(truth_value);
     }
- 
     // put trial together using either the custom radio button plugin or the custom slider plugin, dependent on response format assignment
     if (responseformat_assignment== "radio") {
         // make trials using custom radio button plugin
@@ -643,12 +639,11 @@ function make_test_trial(target_content_type) {
                     training_trial_counter: "NA",
                     test_trial_counter: ++test_trial_counter, 
                     target_truth_value: target_truth_value,
-                    all_TVs: all_TVs, 
+                    all_TVs: all_TVs,
                     target_content_type: target_content_type, 
                     target_image: target_image_filename,
                     images_in_order: images_to_display, 
                     // saves the filenames in the order they were presented in a trial
-                    stimulus: trial_stim.prompt_name
                 };
             },
             on_finish: function (data) {
@@ -667,9 +662,9 @@ function make_test_trial(target_content_type) {
         var slider_trial = {
             type: jsPsychImageArraySliderResponse,
             images: images_to_display,
+            prompt: "",
             labels: response_options,
             highlighted_image_index: index,
-            prompt: "",
             // slider_width: // can set this in pixels if desired
 
             //at the start of the trial, make a note of all relevant info to be saved
@@ -686,8 +681,7 @@ function make_test_trial(target_content_type) {
                     all_TVs: all_TVs,
                     target_content_type: target_content_type,
                     target_image: target_image_filename,
-                    images_in_order: images_to_display, 
-                    stimulus: trial_stim.prompt_name
+                    images_in_order: images_to_display
                 };
             },
             on_finish: function (data) {
@@ -765,18 +759,14 @@ var write_headers = {
         \"test_trial_count\",\
         \"trial_index\",\
         \"target_TV\",\
-        \"TV1\",\
-        \"TV2\",\
-        \"TV3\",\
-        \"TV4\",\
+        \"allTVs\",\
         \"target_content_type\",\
-//      \"prompt\",\
+        \"prompt\",\
         \"target_image\",\
         \"images_in_presentation_order_0\",\
         \"images_in_presentation_order_1\",\
         \"images_in_presentation_order_2\",\
         \"images_in_presentation_order_3\",\
-        \"stimName\",\
         \"response\",\
         \"time_elapsed\",\
         \"rt\"\n"
